ff39fbc0d8740d3d7ec0bacfa4f30fab869f9fec
Mana <manakouwenberg@gmail.com>
2020-08-28 13:02:27 +0200

    Nice

---
+++ Blimp Game/Assets/Scripts/Blimp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blimp : MonoBehaviour
+{
+    float leftPropSpeed, rightPropSpeed;
+    public GameObject leftPropellor, rightPropellor;
+    Rigidbody rb;
+    public float propRotSpeed;
+    public float blimpRotSpeed, blimpMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePropellors();
+        ControlBlimp();
+    }
+
+    void RotatePropellors()
+    {
+        leftPropellor.transform.localRotation *= Quaternion.AngleAxis(propRotSpeed * Time.deltaTime * leftPropSpeed, Vector3.forward);
+        rightPropellor.transform.localRotation *= Quaternion.AngleAxis(propRotSpeed * Time.deltaTime * rightPropSpeed, Vector3.forward);
+    }
+
+    void ControlBlimp()
+    {
+        rb.MoveRotation(transform.rotation * Quaternion.AngleAxis(blimpRotSpeed * Time.deltaTime * (leftPropSpeed - rightPropSpeed), transform.up));
+        rb.AddForce(transform.forward * ((leftPropSpeed + rightPropSpeed) * blimpMoveSpeed));
+    }
+
+    public void SetLeftPropellor(float strength)
+    {
+        leftPropSpeed = strength;
+    }
+
+    public void SetRightPropellor(float strength)
+    {
+        rightPropSpeed = strength;
+    }
+}
---
+++ Blimp Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Camera thirdperson, firstPerson;
+    bool inThirdPerson;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inThirdPerson = thirdperson.enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V)) { SwitchCameras(); }
+    }
+
+    void SwitchCameras()
+    {
+        thirdperson.enabled = !inThirdPerson;
+        firstPerson.enabled = inThirdPerson;
+        inThirdPerson = thirdperson.enabled;
+    }
+}
